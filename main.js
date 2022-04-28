d3088ed92e095b02f15848524a9848d85df9816e
Luca <vintiluca@gmail.com>
2022-04-28 17:31:50 +0200

    Karte mit Startlayer und Stephansdom einfügen

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 /* OGD Wien Beispiel */
 
+
 let stephansdom = {
 lat: 48.208493,
 lng: 16.373118,
 title: "Stephansdom"
 };
+
+let startLayer = L.titeLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [ stephansdom.lat, stephansdom.lng],
+    zoom: 12,
+    layers: [
+        startLayer
+    ],
+});
+
