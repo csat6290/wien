7eaf570d5487e1ad38b9b56b2b6254add6c17fb3
Luca <vintiluca@gmail.com>
2022-04-28 17:54:44 +0200

    beautify

--- main.js
+++ main.js
@@ -2,49 +2,48 @@
 
 
 let stephansdom = {
-lat: 48.208493,
-lng: 16.373118,
-title: "Stephansdom"
+    lat: 48.208493,
+    lng: 16.373118,
+    title: "Stephansdom"
 };
 
 let startLayer = L.tileLayer.provider("BasemapAT.grau");
 
 let map = L.map("map", {
-    center: [ stephansdom.lat, stephansdom.lng],
+    center: [stephansdom.lat, stephansdom.lng],
     zoom: 12,
     layers: [
         startLayer
     ],
 });
 
-let layerControl = L.control.layers(
-    {
-        "BasemapAT Grau": startLayer,
-       
-        "Basemap Standard": L.tileLayer.provider("BasemapAT.basemap"),
-       
-        "Basemap High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
-       
-        "Basemap Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
-    
-        "Basemap Gelände": L.tileLayer.provider("BasemapAT.terrain"),
-        
-        "Basemap Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
-        
-        "Basemap Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+let layerControl = L.control.layers({
+    "BasemapAT Grau": startLayer,
 
-        "Basemap mit Orthofot und Beschriftung": L.layerGroup([
-            L.tileLayer.provider("BasemapAT.orthofoto"),
-            L.tileLayer.provider("BasemapAT.overlay"),
-        ])
-    }).addTo(map);
+    "Basemap Standard": L.tileLayer.provider("BasemapAT.basemap"),
 
-    layerControl.expand();
+    "Basemap High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
 
-    let sightLayer = L.featureGroup();
+    "Basemap Beschriftung": L.tileLayer.provider("BasemapAT.overlay"),
 
-    layerControl.addOverlay(sightLayer, "Sehenswürdigkeiten");
+    "Basemap Gelände": L.tileLayer.provider("BasemapAT.terrain"),
 
-    let mkr = L.marker([
-        stephansdom.lat, stephansdom.lng
-    ]).addTo(sightLayer);
+    "Basemap Oberfläche": L.tileLayer.provider("BasemapAT.surface"),
+
+    "Basemap Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+
+    "Basemap mit Orthofot und Beschriftung": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay"),
+    ])
+}).addTo(map);
+
+layerControl.expand();
+
+let sightLayer = L.featureGroup();
+
+layerControl.addOverlay(sightLayer, "Sehenswürdigkeiten");
+
+let mkr = L.marker([
+    stephansdom.lat, stephansdom.lng
+]).addTo(sightLayer);
