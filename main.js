f80915edcdfff0f3e654ee752e7205a226b9b99c
Luca <vintiluca@gmail.com>
2022-05-05 16:38:06 +0200

    Fußgängerzonen implementiert

--- main.js
+++ main.js
@@ -114,3 +114,18 @@ async function loadLines(url) {
 }
 
 loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
+// Fußgängerzonen Vienna Sightseeing
+async function loadZones(url) {
+    let response = await fetch(url);
+    let geojson = await response.json();
+    //console.log(geojson);
+
+    let overlay = L.featureGroup();
+    layerControl.addOverlay(overlay, "Fußgängerzonen");
+    overlay.addTo(map);
+
+    L.geoJSON(geojson).addTo(overlay)
+}
+
+loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
