01691726b55eeb4dbae8a74c124907caecd14739
Luca <vintiluca@gmail.com>
2022-05-05 17:00:48 +0200

    beautify

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ L.control.fullscreen().addTo(map);
 //map.addControl(L.control.fullscreen());
 
 let miniMap = new L.Control.MiniMap(
-    L.tileLayer.provider("BasemapAT"),{
+    L.tileLayer.provider("BasemapAT"), {
         toggleDisplay: true
     }
 ).addTo(map);
