9854325f87191f6e23f8bbbf5b7df55237477152
Luca <vintiluca@gmail.com>
2022-05-05 17:03:00 +0200

    alle layer außer Sehenswürdigkeiten deaktivierten

--- main.js
+++ main.js
@@ -98,7 +98,7 @@ async function loadStops(url) {
     L.geoJSON(geojson).addTo(overlay)
 }
 
-loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSBIGOGD&srsName=EPSG:4326&outputFormat=json");
+//loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSBIGOGD&srsName=EPSG:4326&outputFormat=json");
 
 // Liniennetz Vienna Sightseeing
 async function loadLines(url) {
@@ -113,7 +113,7 @@ async function loadLines(url) {
     L.geoJSON(geojson).addTo(overlay)
 }
 
-loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+//loadLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
 
 // Fußgängerzonen Vienna Sightseeing
 async function loadZones(url) {
@@ -128,7 +128,7 @@ async function loadZones(url) {
     L.geoJSON(geojson).addTo(overlay)
 }
 
-loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
+//loadZones("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json");
 
 // Hotels und Unterkünfte 
 async function loadHotels(url) {
@@ -143,4 +143,4 @@ async function loadHotels(url) {
     L.geoJSON(geojson).addTo(overlay)
 }
 
-loadHotels("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:UNTERKUNFTOGD&srsName=EPSG:4326&outputFormat=json");
+//loadHotels("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:UNTERKUNFTOGD&srsName=EPSG:4326&outputFormat=json");
