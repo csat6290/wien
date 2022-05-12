9135b9829f99c14aa9b6fab4b63cc6cd3c99d192
Luca <vintiluca@gmail.com>
2022-05-12 18:11:08 +0200

    alle HotelMarker durchgehen und Eigenschaften anzeigen

--- main.js
+++ main.js
@@ -216,7 +216,7 @@ async function loadHotels(url) {
     layerControl.addOverlay(overlay, "Hotels und Unterkünfte");
     overlay.addTo(map);
 
-    L.geoJSON(geojson, {
+    let hotelsLayer = L.geoJSON(geojson, {
         pointToLayer: function (geoJsonPoint, latlng) {
             
             //searchList machen mit der Console
@@ -277,6 +277,12 @@ async function loadHotels(url) {
     let form = document.querySelector("#searchForm");
     form.suchen.onclick = function() {
         console.log(form.hotel.value);
+        hotelsLayer.eachLayer(function(marker){
+            console.log(marker)
+            console.log(marker.getLatLng())
+            console.log(marker.getPopup())
+            console.log(marker.feature.properties.BETRIEB)
+        })
     }
 
 }
